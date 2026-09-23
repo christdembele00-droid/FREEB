3a15e60563377a621177d7d5fed2e9608e61aa4c
christdembele00-droid <christdembele00@gmail.com>
2026-09-23 14:38:39 +0000

    chore: scaffold FREEB architecture (build.gradle.kts)

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+}
+
+tasks.register("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
